bf5a142532f31add542e3abec508704eff34dcd1
JeffreyJadueWang <wangjiaju960914@163.com>
2026-02-28 13:31:43 +0800

    update fileProcess

--- src/utils/filesProcess.py
+++ src/utils/filesProcess.py
@@ -5,7 +5,6 @@ class FileProcess:
         工具类：
             1. 提供判断文件夹内的文件数的工具函数
             2. 提供判断文件大小的函数
-            3. 提供判断硬件资源的函数，包括：CPU、线程、内存资源等
     """
 
     def __init__(self,file_dir):
@@ -30,7 +29,4 @@ class FileProcess:
         """
 
         return os.stat(raw_file).st_size / 1024 / 1024
-
-    def hardware_resources(self):
-        pass
 
