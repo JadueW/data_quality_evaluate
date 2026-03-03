85cdf7e4082c2142d45a45bc2fe9258300d9cf1a
liang <707064754@qq.com>
2026-03-03 15:45:41 +0800

    modify processed data structure

--- src/analyse.py
+++ src/analyse.py
@@ -7,6 +7,7 @@ import numpy as np
 from .metrics.calc_snr import compute_single_window_snr
 from .preprocessing.preprocessor import Preprocessor
 
+
 def handle_statistics(batch_datasets):
     """
     针对统计量指标，进行数据处理
@@ -28,11 +29,11 @@ def handle_statistics(batch_datasets):
             continue
         ds_template.update({k: v})
     # batch data的预处理模板
-    _template = {
-        "win_check_mask": [],
-        "ch_check_mask": [],
-        "processed_data": []
-    }
+    # _template = {
+    #     "win_check_mask": [],
+    #     "ch_check_mask": [],
+    #     "processed_data": []
+    # }
 
     # 分窗口
     win_size, overlap, wid, s = 5, 0, 0, 0
@@ -52,17 +53,18 @@ def handle_statistics(batch_datasets):
             connector_mapping=None,  # 以下参数均为分组时使用的参数，在ele_type为pse-XX时起效
             pse_num=1,
             pse_order="order",
-            pse_ch_num="4",
+            pse_ch_num=4,
         )
+        grouped_processed_data.update({wid: _processed_data})
         # 将处理完的窗口数据分组保存
-        for k, v in _processed_data.items():
-            if k not in grouped_processed_data:
-                grouped_processed_data.update({
-                    k: _template
-                })
-            grouped_processed_data[k]["win_check_mask"].append(v["is_good"])
-            grouped_processed_data[k]["ch_check_mask"].append(v["ch_check_mask"])
-            grouped_processed_data[k]["processed_data"].append(v["processed_data"])
+        # for k, v in _processed_data.items():
+        #     if k not in grouped_processed_data:
+        #         grouped_processed_data.update({
+        #             k: _template
+        #         })
+        #     grouped_processed_data[k]["win_check_mask"].append(v["is_good"])
+        #     grouped_processed_data[k]["ch_check_mask"].append(v["ch_check_mask"])
+        #     grouped_processed_data[k]["processed_data"].append(v["processed_data"])
 
         s = e
         wid += 1
@@ -90,11 +92,11 @@ def handle_snr(batch_datasets):
             continue
         ds_template.update({k: v})
     # batch data的预处理模板
-    _template = {
-        "win_check_mask": [],
-        "ch_check_mask": [],
-        "win_SNR": []
-    }
+    # _template = {
+    #     "win_check_mask": [],
+    #     "ch_check_mask": [],
+    #     "win_SNR": []
+    # }
 
     # 分窗口
     win_size, overlap, wid, s = 60, 30, 0, 0
@@ -114,26 +116,44 @@ def handle_snr(batch_datasets):
             connector_mapping=None,  # 以下参数均为分组时使用的参数，在ele_type为pse-XX时起效
             pse_num=1,
             pse_order="order",
-            pse_ch_num="4",
+            pse_ch_num=4,
         )
-        # 将处理完的窗口数据分组保存
+
+        # 计算每组数据的SNR
         for k, v in _processed_data.items():
-            if k not in grouped_snr:
-                grouped_snr.update({
-                    k: _template
-                })
             is_good = v["is_good"]
+
             if is_good:
                 snr_info = compute_single_window_snr(v["processed_data"], batch_datasets["fs"])
-                if not snr_info:
-                    grouped_snr[k]["win_SNR"].append(snr_info["snr"])
+                # 并更新snr
+                if not np.all(snr_info):
+                    v.update({"win_SNR": snr_info["snr"]})
                 else:
-                    grouped_snr[k]["win_SNR"].append(np.zeros((-1, pp.group_ch_num)))
+                    v.update({"win_SNR": np.zeros((-1, pp.group_ch_num))})
             else:
-                grouped_snr[k]["win_SNR"].append(np.zeros((-1, pp.group_ch_num)))
+                v.update({"win_SNR": np.zeros((-1, pp.group_ch_num))})
 
-            grouped_snr[k]["win_check_mask"].append(is_good)
-            grouped_snr[k]["ch_check_mask"].append(v["ch_check_mask"])
+            # 去掉预处理数据
+            del v["processed_data"]
+        grouped_snr.update({wid: _processed_data})
+        # 将处理完的窗口数据分组保存
+        # for k, v in _processed_data.items():
+        #     if k not in grouped_snr:
+        #         grouped_snr.update({
+        #             k: _template
+        #         })
+        #     is_good = v["is_good"]
+        #     if is_good:
+        #         snr_info = compute_single_window_snr(v["processed_data"], batch_datasets["fs"])
+        #         if not snr_info:
+        #             grouped_snr[k]["win_SNR"].append(snr_info["snr"])
+        #         else:
+        #             grouped_snr[k]["win_SNR"].append(np.zeros((-1, pp.group_ch_num)))
+        #     else:
+        #         grouped_snr[k]["win_SNR"].append(np.zeros((-1, pp.group_ch_num)))
+        #
+        #     grouped_snr[k]["win_check_mask"].append(is_good)
+        #     grouped_snr[k]["ch_check_mask"].append(v["ch_check_mask"])
 
         s = s + int(overlap * batch_datasets["fs"])
         wid += 1
