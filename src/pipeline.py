9352d4cecb3c9818383f01a774c25786853ce03d
JeffreyJadueWang <wangjiaju960914@163.com>
2026-03-05 13:57:36 +0800

    update pipeline.py: 更新plt.rcParams的相关参数

--- src/pipeline.py
+++ src/pipeline.py
@@ -15,6 +15,73 @@ from src.report.extractReportFeatures import ExtractReportFeatures
 from src.report.report_generator import PDFReportGenerator
 from src.visualize.visualizer import Visualizer
 from src.preprocessing.preprocessor import Preprocessor
+import matplotlib.pyplot as plt
+import matplotlib
+
+plt.rcParams.update({
+    # --- 字体设置 ---
+    'font.family': 'serif',
+    'font.serif': ['Times New Roman', 'DejaVu Serif', 'serif'],
+    'mathtext.fontset': 'stix',
+    'mathtext.rm': 'Times New Roman',
+    'mathtext.it': 'Times New Roman:italic',
+    'mathtext.bf': 'Times New Roman:bold',
+    'axes.unicode_minus': False,
+
+    # --- 字号设置---
+    'font.size': 16,
+    'axes.titlesize': 13,
+    'axes.labelsize': 13,
+    'axes.labelweight': 'bold',
+    'xtick.labelsize': 12,
+    'ytick.labelsize': 12,
+    'legend.fontsize': 12,
+    'legend.title_fontsize': 10,
+    'figure.titlesize': 11,
+
+    # --- 线条和标记 ---
+    'lines.linewidth': 1.2,
+    'lines.markersize': 4,
+    'lines.markeredgewidth': 0.5,
+
+    # 坐标轴样式
+    'axes.spines.top': False,
+    'axes.spines.right': False,
+    'axes.linewidth': 1.0,
+
+    # --- 刻度样式 ---
+    'xtick.major.width': 0.8,
+    'ytick.major.width': 0.8,
+    'xtick.minor.width': 0.4,
+    'ytick.minor.width': 0.4,
+    'xtick.major.size': 3,
+    'ytick.major.size': 3,
+    'xtick.minor.size': 1.5,
+    'ytick.minor.size': 1.5,
+    'xtick.direction': 'in',
+    'ytick.direction': 'in',
+    'xtick.top': False,
+    'ytick.right': False,
+
+    # --- 图例 ---
+    'legend.frameon': True,
+    'legend.edgecolor': 'black',
+    'legend.fancybox': False,
+    'legend.borderpad': 0.4,
+    'legend.handlelength': 1.5,
+
+    # --- 输出 ---
+    'figure.dpi': 300,
+    'savefig.dpi': 600,
+    'savefig.format': 'pdf',
+    'savefig.bbox': 'tight',
+    'savefig.pad_inches': 0.02,
+
+    'axes.prop_cycle': plt.cycler(color=[
+        '#1A2CA3', '#C40C0C', '#2e7d32', '#f57c00',
+        '#6a1b9a', '#00695c', '#ad1457', '#455a64',
+    ]),
+})
 
 
 if __name__ == '__main__':
